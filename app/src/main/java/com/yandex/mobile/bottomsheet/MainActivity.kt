a68c2541c4ded935f07faafe36a3aff02bf11970
Vitaly Zayankovsky <zayankov@yandex-team.ru>
2020-08-11 01:56:02 +0300

    add bottom sheet

--- app/src/main/java/com/yandex/mobile/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/yandex/mobile/bottomsheet/MainActivity.kt
@@ -2,11 +2,17 @@ package com.yandex.mobile.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val behavior = BottomSheetBehavior.from(scroll)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        content.setOnClickListener { behavior.state = BottomSheetBehavior.STATE_EXPANDED }
     }
 }
